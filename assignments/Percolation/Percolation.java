56af516bdc3527bce5680801ffbff26ccce77b3f
Eug Kenny <eugkenny@gmail.com>
2020-10-21 11:04:42 +0100

    Assignment 1

---
+++ assignments/Percolation/Percolation.java
@@ -0,0 +1,37 @@
+/******************************************************************************
+* Data Structures Assignment 1
+*
+* Name: YOUR NAME HERE
+* ID: YOUR K NUMBER HERE
+* Course: YOUR COURSE HERE
+*
+******************************************************************************/
+public class Percolation{
+	
+	public Percolation( int N){
+		// YOUR CODE HERE
+	}
+
+	public void open( int i, int j){
+		// YOUR CODE HERE
+	}
+	
+	public boolean isOpen( int i, int j){
+		// YOUR CODE HERE
+	}
+	
+	public boolean isFull( int i, int j){
+		// YOUR CODE HERE
+	}
+	
+	public boolean percolates(){
+		// YOUR CODE HERE
+	}
+	
+	public static void main(String [] args){
+	    int N = 5
+	    Percolation p = new Percolation(N);
+	    
+	    
+	}
+}
